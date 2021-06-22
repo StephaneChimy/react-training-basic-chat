043e07c5ead228094f48fb832864b1b49983271a
Stéphane Chimy <stephane.akira@gmail.com>
2021-06-22 10:42:02 +0200

    Added colors of brand

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors")
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors:{
-      transparent: 'transparent',
-      current: 'currentColor',
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
       gray: colors.trueGray,
       red: colors.red,
       blue: colors.sky,
       yellow: colors.amber,
       teal: colors.teal,
-      emerald: colors.emerald
+      emerald: colors.emerald,
+      grayBrand:{
+        DEFAULT: '#A1B1B6'
+      },
+      greenBrand: {
+        light: "#97BAB2",
+        DEFAULT: "#43636D",
+      },
+      yellowBrand: {
+        light: '#FCE2B3',
+        DEFAULT: "#F9C667",
+      },
     },
     extend: {},
   },
