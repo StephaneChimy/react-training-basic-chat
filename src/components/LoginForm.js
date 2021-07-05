8a08b23a8c41fd2e8a6e6fb8e364ab7f876e30b4
Stéphane Chimy <stephane.akira@gmail.com>
2021-07-05 12:37:08 +0200

    changed input focus color to yelowBrand

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -35,7 +35,7 @@ const LoginForm = () => {
                       <input
                         type='text'
                         id='sign-in-email'
-                        class=' rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent'
+                        class=' rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-yellowBrand focus:border-transparent'
                         placeholder='Your email'
                       />
                     </div>
@@ -50,7 +50,7 @@ const LoginForm = () => {
                       <input
                         type='password'
                         id='sign-in-email'
-                        class=' rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent'
+                        class=' rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-yellowBrand focus:border-transparent'
                         placeholder='Your password'
                       />
                     </div>
--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,25 +1,25 @@
 import firebase from "firebase/app"
-import { auth} from "./firebase"
+import { auth } from "./firebase"
 
 const signInWithGoogle = async () => {
-    // retrieve Google provider object
-    const provider = new firebase.auth.GoogleAuthProvider();
-    // Set language to the default browser preference
-    auth.useDeviceLanguage()
-    // Start sign in process
-    try {
-      await auth.signInWithPopup(provider)
-    } catch (error) {
-      console.error(error)
-    }
+  // retrieve Google provider object
+  const provider = new firebase.auth.GoogleAuthProvider()
+  // Set language to the default browser preference
+  auth.useDeviceLanguage()
+  // Start sign in process
+  try {
+    await auth.signInWithPopup(provider)
+  } catch (error) {
+    console.error(error)
   }
-  
-  const signOut = async () => {
-    try {
-      await auth.signOut()
-    } catch (error) {
-      console.log(error.error)
-    }
-  }
+}
 
-export {signInWithGoogle, signOut}
+const signOut = async () => {
+  try {
+    await auth.signOut()
+  } catch (error) {
+    console.log(error.error)
+  }
+}
+
+export { signInWithGoogle, signOut }
